fe3198fb0cf357052d20e24a5bea4e36c6d15805
THEKIV <thekiv@mail.ru>
2024-05-18 11:56:19 +0300

    Add files via upload

---
+++ scr/no_img.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+$('img').error(function() {
+$(this).attr({src: 'https://v1.iconsearch.ru/ajax/download.php?icon_id=41232&size=3&format=png', style:'width:32px;height:32px;'});
+});
+});
