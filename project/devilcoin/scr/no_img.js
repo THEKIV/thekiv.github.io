1046496cd2d368ea11095a28d6a192e4d88c5fb5
THEKIV <thekiv@mail.ru>
2024-05-26 20:48:59 +0300

    Delete project/devilcoin directory

--- project/devilcoin/scr/no_img.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-$('img').error(function() {
-$(this).attr({src: 'https://v1.iconsearch.ru/ajax/download.php?icon_id=41232&size=3&format=png', style:'width:32px;height:32px;'});
-});
-});
